04ca514707cfaddc9223d8390d42ac166d34062f
Yury Cavalcante <yurypcfarias@gmail.com>
2018-12-06 22:13:33 -0200

    updated sql script

--- src/main/java/senacpi/hospitaltades/utils/script.sql
+++ src/main/java/senacpi/hospitaltades/utils/script.sql
@@ -85,4 +85,32 @@ CREATE TABLE `consultas` (
 );
 
 INSERT INTO `usuario` (idUsuario, login, senha, nome, cargo, loginAtivo, codFilial, ativo)
-VALUES (1, 'yurypcf', 123456, 'Yury', 'Admin', 1, '23A', 1)
+VALUES (1, 'yurypcf', 123456, 'Yury', 'Admin', 1, '23A', 1);
+INSERT INTO `usuario` (idUsuario, login, senha, nome, cargo, loginAtivo, codFilial, ativo)
+VALUES (2, 's.santos', 123456, 'Sabrina', 'Medico', 1, '23A', 1);
+INSERT INTO `usuario` (idUsuario, login, senha, nome, cargo, loginAtivo, codFilial, ativo)
+VALUES (3, 'p.sato', 123456, 'Pedro', 'Medico', 1, '23B', 1);
+INSERT INTO `usuario` (idUsuario, login, senha, nome, cargo, loginAtivo, codFilial, ativo)
+VALUES (4, 'f.pacheco', 123456, 'Fernanda', 'Atendente', 1, '23B', 1);
+INSERT INTO `usuario` (idUsuario, login, senha, nome, cargo, loginAtivo, codFilial, ativo)
+VALUES (5, 'k.neres', 123456, 'Kelvin', 'Atendente', 1, '23A', 1);
+
+INSERT INTO `paciente` (id, nome, sobrenome, dataNasc, cpf, sexo, contato, email, codFilial, ativo)
+VALUES (1, 'Claudio', 'Filho', '1992-09-22', '22222878521', 'Masculino', '11925368741', 'c.farias@gmail.com', '23A', 1);
+INSERT INTO `paciente` (id, nome, sobrenome, dataNasc, cpf, sexo, contato, email, codFilial, ativo)
+VALUES (2, 'Luana', 'Alves', '1991-10-22', '22222878521', 'Masculino', '11925368741', 'l.alves@gmail.com', '23B', 1);
+
+INSERT INTO `medico` (idMedico, nome, sobrenome, cpf, sexo, crm, idUsuario, codFilial, ativo)
+VALUES (1, 'Sabrina', 'Santos', '45117536244', 'Feminino', '238415788BR', 1, '23A', 1);
+INSERT INTO `medico` (idMedico, nome, sobrenome, cpf, sexo, crm, idUsuario, codFilial, ativo)
+VALUES (3, 'Pedro', 'Sato', '45117536244', 'Masculino', '238413348BR', 3, '23B', 1);
+
+INSERT INTO `atendente` (idAtendente, nome, sobrenome, cpf, sexo, idUsuario, codFilial, ativo)
+VALUES (1, 'Kelvin', 'Neres', '22784661425', 'Masculino', 5, '23A', 1);
+INSERT INTO `atendente` (idAtendente, nome, sobrenome, cpf, sexo, idUsuario, codFilial, ativo)
+VALUES (4, 'Fernanda', 'Pacheco', '22784661425', 'Masculino', 4, '23B', 1);
+
+INSERT INTO `remedio` (idRemedio, nome, quantidade, codFilial, ativo)
+VALUES (1, 'Paracetamol', '200', '23A', 1);
+INSERT INTO `remedio` (idRemedio, nome, quantidade, codFilial, ativo)
+VALUES (2, 'Dipirona Infantil', '400', '23B', 1);
